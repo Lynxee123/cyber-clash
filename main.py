511db79857c4e9e8507a29709d129cfdfbb8ab2d
lili <lilianmill20@gmail.com>
2025-11-03 16:38:50 -0800

    main loop

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ pygame.init()
 # Customize game window
 screen_width = 800
 screen_height = 600
-background = (255,255,255)
+background = (255,2,255)
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("cyber-clash")
 
 # Main game loop 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def main():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            #elif 
 
-    screen.fill(background)
-    # Update full display Surface to the screen
-    pygame.display.flip()
+        screen.fill(background)
+        # Update full display Surface to the screen
+        pygame.display.flip()
 
+main()
